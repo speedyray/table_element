81550fab93816d47ea5379d14fbeb6b81968ac50
Raynold Gyasi <speedyray2ray@gmail.com>
2019-02-06 11:07:58 -0500

    table elements

---
+++ src/Data.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Data = () => {
+   return(
+    <table>
+         <tbody>
+            <tr> 
+                <td>
+                    <h1>Superman </h1> 
+                </td>
+                <td>
+                   <h1>  Lee</h1> 
+                </td>
+                <td>
+                   <h1>  50</h1> 
+                </td>
+            </tr>
+         </tbody>
+    
+    </table>);
+    
+          
+}
+
+
+
+
+
+
+
+export default Data;
---
+++ src/Table.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Data from './Data';
+// Function component to return array with any number of elements in it
+// function App() {
+//   let myarr = [ 3, 4, 7, 8, 9, 10 ];
+//   let splitArray = myarr.join( ' ');
+//   return(<div>
+//           {splitArray}
+//          </div>
+//           )
+//       }
+
+
+function Table() {
+        
+       return(
+              < Data />
+              
+              )
+         }
+
+
+
+
+export default Table;
